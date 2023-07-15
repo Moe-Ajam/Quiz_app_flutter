d69477ee54fbb64f1d59e95788151911398b8717
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2023-07-15 22:09:01 +0300

    Adding dynamic list

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -6,4 +6,10 @@ class QuizQuestion {
 
   const QuizQuestion(this.text, this.answers);
 
+  List<String> getShuffledList(){
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
+
 }
